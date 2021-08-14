33539e3c2ecba129984179f784c74fc7de7e110d
Gurol Bilgin <gurolbilgin@gmail.com>
2021-08-14 14:53:47 -0600

    adds the running version of the project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cards from './components/cards/Cards';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Cards />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="aboutus">about us</a>
+      <a href="foryou">for you</a>
+      <a href="services">services</a>
+      <a href="blog">blog</a>
+      <a href="vlog">vlog</a>
+      <a href="contact">contact us</a>
+      
+    </div>
+  )
+}
+
+export default Navbar
